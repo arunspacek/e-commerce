4bea5ab0a786004c65d0782f2dc2de55b7778f6a
arun <arunaaron243@gmail.com>
2022-06-02 23:33:18 +0530

    New updates

--- myshop/settings.py
+++ myshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$qfe-6h*qjzf!1gk3-bxl^o^)c--yi9p*)krelo$r7o-j_t9b+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
